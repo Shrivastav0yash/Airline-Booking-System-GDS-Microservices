c5e8251ebff3cb8538c5e25f6fda625c17555c85
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-06-16 01:59:08 +0530

    Creating location service

---
+++ services/location-service/src/test/java/com/microservice/airline/location_service/LocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.airline.location_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
